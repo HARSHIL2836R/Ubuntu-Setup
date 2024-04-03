ba2bc5ffff99366be0f5016bb812446bec142a48
HARSHIL2836R <harshilsolanki1912@gmail.com>
2024-04-03 14:43:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+**To make Ubuntu Full Screen**
+- Device->Insert Guest Additions CD Image...
+- Go to files->Go to CD->Run Software
---
+++ setup.sh
@@ -0,0 +1,24 @@
+# Update apt
+sudo apt update
+
+# Confirm that universe repository is enabled in your system
+sudo add-apt-repository universe
+
+# Install Gnome Tweak Tool
+echo Installing Gnome Twaek Tool
+sudo apt install gnome-tweak-tool
+
+# Install Google Chrome
+echo Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Install VS Code
+echo Installing VS Code
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y
+sudo apt install code
